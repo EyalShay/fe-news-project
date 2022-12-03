986421544373e2f4f66956193a465e9d5d30c0d8
eyal shay <eyalshay@eyals-MacBook-Air.local>
2022-12-03 17:33:03 +0000

    updated api.js to pull data from new backend source

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchArticles = ({ sortBy, orderBy, topic }) => {
   return axios
-    .get(`https://eyal-ncnews.herokuapp.com/api/articles`, {
+    .get(`https://eyalshay-nc-news-be.cyclic.app/api/articles`, {
       params: { sort_by: sortBy, order: orderBy, topic: topic },
     })
     .then((res) => {
@@ -12,42 +12,44 @@ export const fetchArticles = ({ sortBy, orderBy, topic }) => {
 
 export const fetchArticle = (article_id) => {
   return axios.get(
-    `https://eyal-ncnews.herokuapp.com/api/articles/${article_id}`
+    `https://eyalshay-nc-news-be.cyclic.app/api/articles/${article_id}`
   );
 };
 
 export const fetchTopics = () => {
-  return fetch("https://eyal-ncnews.herokuapp.com/api/topics").then((res) => {
-    return res.json();
-  });
+  return fetch("https://eyalshay-nc-news-be.cyclic.app/api/topics").then(
+    (res) => {
+      return res.json();
+    }
+  );
 };
 
 export const fetchUsers = () => {
-  return axios.get(`https://eyal-ncnews.herokuapp.com/api/users`);
+  return axios.get(`https://eyalshay-nc-news-be.cyclic.app/api/users`);
 };
 
 export const fetchComments = (article_id) => {
   return axios.get(
-    `https://eyal-ncnews.herokuapp.com/api/articles/${article_id}/comments`
+    `https://eyalshay-nc-news-be.cyclic.app/api/articles/${article_id}/comments`
   );
 };
 
 export const patchArticle = (article_id, vote_count) => {
   return axios.patch(
-    `https://eyal-ncnews.herokuapp.com/api/articles/${article_id}`,
+    `https://eyalshay-nc-news-be.cyclic.app/api/articles/${article_id}`,
     { inc_votes: vote_count }
   );
 };
 
 export const postComment = (article_id, newComment) => {
   return axios.post(
-    `https://eyal-ncnews.herokuapp.com/api/articles/${article_id}/comments`,
+    `https://eyalshay-nc-news-be.cyclic.app/api/articles/${article_id}/comments`,
     newComment
   );
 };
 
 export const deleteCommentById = (comment_id) => {
   return axios.delete(
-    `https://eyal-ncnews.herokuapp.com/api/comments/${comment_id}`
+    `https://eyalshay-nc-news-be.cyclic.app/api/comments/${comment_id}`
   );
 };
